153a81599bdf5654fbde5fb86df416beec4d52db
Morel Adam <adam.morel@etudiant.univ-rennes1.fr>
2021-08-13 09:38:14 +0200

    update contract 1

--- contracts/seedz.sol
+++ contracts/seedz.sol
@@ -6,6 +6,7 @@ contract seedz {
 	string  public name = "Seeds Token";
     string  public symbol = "SEEDZ";
     string  public standard = "seeds token v0.1";
+    bool public investing = false; //TODO should be in the account/UI not coin ?
 
 	constructor (uint256 _initialSupply) public{
 		totalSupply = _initialSupply;
@@ -28,13 +29,46 @@ contract seedz {
     mapping(address => uint256) public balanceOf;
     mapping(address => mapping(address => uint256)) public allowance;
 
-function transfer(address _to, uint256 _value) public returns (bool success) {
+    function transfer(address _to, uint256 _value) public returns (bool success) {
         require(balanceOf[msg.sender] >= _value);
 
         balanceOf[msg.sender] -= _value;
         balanceOf[_to] += _value;
 
-        emit Transfer(msg.sender, _to, _value); //condition ici
+        emit Transfer(msg.sender, _to, _value); 
+
+        return true;
+    }
+
+    function transfer(address _to_invfund, address _to, uint256 _value, uint decimal) public returns (bool success) {
+
+        if(investing){
+            uint256 _val2 = _value - (_value % decimal *10); 
+            transfer_off(_to_invfund,_val2);}
+        
+        transfer_normal(_to, _value);
+
+        return true;
+    }
+
+    function transfer_off(address _to, uint256 _value) public returns (bool success) {
+        require(balanceOf[msg.sender] >= _value);
+
+        balanceOf[msg.sender] -= _value;
+        balanceOf[_to] += _value;
+
+        //emit Transfer(msg.sender, _to, _value); //condition ici
+
+        return false;
+    }
+
+    function transfer_normal(address _to, uint256 _value) public returns (bool success) {
+        require(balanceOf[msg.sender] >= _value);
+
+        balanceOf[msg.sender] -= _value;
+        balanceOf[_to] += _value;
+
+        emit Transfer(msg.sender, _to, _value); 
 
         return true;
     }
